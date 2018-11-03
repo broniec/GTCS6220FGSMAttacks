fd56cb2d8614b8d1ad7d2868bc2dea7dccf76296
Adithya Nott <anott3@gatech.edu>
2018-11-03 14:29:43 -0400

    98% accuracy

--- src/experiment.py
+++ src/experiment.py
@@ -2,21 +2,21 @@ import semeion as s
 import torch
 
 # batch_sizes = [1, 2, 4, 8, 16, 32, 64]
-learning_rates = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05]
-linear_layer_sizes = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+learning_rates = [0.02, 0.03, 0.05, 0.07, 0.09]
+linear_layer_sizes = [50, 55, 60, 65, 70, 75, 80]
 
 
-# def run_experiment():
-#     for bs in [64]:
-#         for lr in learning_rates:
-#             for lls in linear_layer_sizes:
-#                 print(bs, lr, lls)
-#                 s.run(bs, 1000, lr, lls)
+def run_experiments():
+    for bs in [64]:
+        for lr in learning_rates:
+            for lls in linear_layer_sizes:
+                print(bs, lr, lls)
+                s.run(bs, 1000, lr, lls)
 
 
 def run_experiment():
-    model = s.run(64, 333, 0.05, 65)
-    torch.save(model, 'SemeionCNN')
+    model = s.run(64, 327, 0.0425, 75)
+    torch.save(model, 'SemeionCNN98')
 
 
 if __name__ == '__main__':
